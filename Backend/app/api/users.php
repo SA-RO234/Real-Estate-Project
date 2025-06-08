a7637d6d3f57f8f44063897c5ed00c884537d193
Sun Rosa <rozasun61@gmail.com>
2025-06-09 00:44:05 +0700

    users api

--- Backend/app/api/users.php
+++ Backend/app/api/users.php
@@ -1,9 +1,16 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Fixed header (correct name and removed spaces)
+header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+require_once __DIR__ . '/../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->safeLoad();
+
 require "../controllers/UserController.php";
 require_once "../core/Session.php";
 
@@ -13,10 +20,6 @@ $method = $_SERVER['REQUEST_METHOD'];
 $url = $_SERVER['REQUEST_URI'];
 
 $routes = [
-    "OPTIONS" => function () {
-        http_response_code(200);
-        exit;
-    },
     "GET" => function () use ($usersController) {
         if (isset($_GET['role'])) {
             $usersController->getUserByRole($_GET['role']);
