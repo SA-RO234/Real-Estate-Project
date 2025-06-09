937a8bcf4248571e404e07e83c256c6fea9c2441
Sun Rosa <rozasun61@gmail.com>
2025-06-09 08:15:54 +0700

    Chat Model

--- Backend/app/models/ChatModel.php
+++ Backend/app/models/ChatModel.php
@@ -23,7 +23,7 @@ class ChatModel
             FROM messages
             WHERE (sender_id = ? AND receiver_id = ?)
                OR (sender_id = ? AND receiver_id = ?)
-            ORDER BY sent_at ASC
+            ORDER BY created_at ASC
         ");
 
         $stmt->execute([$sender_id, $receiver_id, $receiver_id, $sender_id]);
@@ -36,13 +36,13 @@ class ChatModel
         $stmt = $this->db->prepare("
          SELECT messages.id, users.name AS sender_name,
                 users.avatar,messages.content AS preview,
-                messages.sent_at,messages.is_read ,
+                messages.created_at,messages.is_read ,
                 messages.sender_id , messages.receiver_id ,
                 messages.is_Active
          FROM messages
          JOIN users ON messages.sender_id = users.id
          WHERE messages.receiver_id = :admin_id
-         ORDER BY messages.sent_at DESC;
+         ORDER BY messages.created_at DESC;
         ");
         $stmt->bindParam(':admin_id', $admin_id, PDO::PARAM_INT);
         $stmt->execute();
@@ -63,7 +63,7 @@ class ChatModel
     public function getAllMessagesDetailFromClient($admin_id, $client_id)
     {
         $stmt = $this->db->prepare("
-        SELECT messages.id, messages.content, messages.sent_at AS time,
+        SELECT messages.id, messages.content, messages.created_at AS time,
                sender.name AS sender_name, sender.avatar AS sender_avatar, sender.role AS sender_role ,
                receiver.name AS receiver_name, receiver.avatar AS receiver_avatar, receiver.role AS receiver_role ,
                messages.is_Active 
@@ -72,7 +72,7 @@ class ChatModel
         JOIN users AS receiver ON messages.receiver_id = receiver.id
         WHERE (messages.sender_id = :client_id AND messages.receiver_id = :admin_id)
            OR (messages.sender_id = :admin_id AND messages.receiver_id = :client_id)
-        ORDER BY messages.sent_at ASC
+        ORDER BY messages.created_at ASC
     ");
 
         $stmt->bindParam(':admin_id', $admin_id, PDO::PARAM_INT);
