6cbb863ba88c2ca6b7453eb137f0c3d24db08e22
Sun Rosa <rozasun61@gmail.com>
2025-06-18 19:39:21 +0700

    Location API

--- Backend/app/api/location.php
+++ Backend/app/api/location.php
@@ -1,6 +1,10 @@
 <?php
-require_once "../controllers/locationController.php";
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Fixed header (correct name and removed spaces)
+header("Access-Control-Allow-Headers: Content-Type");
+
+require_once "../controllers/locationController.php";
 
 $controller = new locationController();
 
